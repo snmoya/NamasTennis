042e7d331091f14f278c0752b82d21d1c09401ed
Sebastian <moya.sebastian@hotmail.com>
2025-01-10 23:33:31 +1000

    Initial commit.

---
+++ NamasTennis/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NamasTennis
+//
+//  Created by Sebastian Moya on 10/1/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NamasTennis/NamasTennisApp.swift
@@ -0,0 +1,17 @@
+//
+//  NamasTennisApp.swift
+//  NamasTennis
+//
+//  Created by Sebastian Moya on 10/1/2025.
+//
+
+import SwiftUI
+
+@main
+struct NamasTennisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
